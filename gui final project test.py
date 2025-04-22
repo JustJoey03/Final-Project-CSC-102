ea428c57056548ebcc921796193515344704efe6
jgaalswijk <justin.gaalswijk@spartans.ut.edu>
2025-04-22 11:26:05 -0400

    Update gui final project test.py
    Loading GUI in proper dimensions and fullscreen

--- gui final project test.py
+++ gui final project test.py
@@ -3,17 +3,26 @@ import threading
 from tkinter import *
 import tkinter
 from bomb_configs import COUNTDOWN, NUM_PHASES, NUM_STRIKES
+from PIL import Image, ImageTk
 
 class BombGUI :
     def __init__(self):
         self.root = Tk()
-        #(message for me) apprentely can change to windowed size using
-        # self.root.attributes('-fullscreen', True)
+        self.root.attributes('-fullscreen', True)
         self.root.geometry('800x480')  
         self.root.configure(cursor='none')
 
+        # Get screen dimensions for proper sizing
+        root = tkinter.Tk()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+
+        # Resizing image
+        image = Image.open("panel.png")
+        resized_image = image.resize((screen_width, screen_height))
+        self.bg_photo = ImageTk.PhotoImage(resized_image)
+    
         # Load background panel (bomb) using Tkinter
-        self.bg_photo = PhotoImage(file='panel.png')
         self.canvas = Canvas(self.root,
                              width=self.bg_photo.width(),
                              height=self.bg_photo.height())
