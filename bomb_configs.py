0509921ef2faaf5c24c3b414babc5fcb9151f7d1
jgaalswijk <justin.gaalswijk@spartans.ut.edu>
2025-04-28 23:24:15 -0400

    Controlled serial number generation
    for toggles, had to edit base bomb_configs because it literally wouldn't work if I did renamed duplicates

--- bomb_configs.py
+++ bomb_configs.py
@@ -88,34 +88,51 @@ if (RPi):
 # functions
 ###########
 # generates the bomb's serial number
-#  it should be made up of alphaneumeric characters, and include at least 3 digits and 3 letters
-#  the sum of the digits should be in the range 1..15 to set the toggles target
+#  it should be made up of alphaneumeric characters, and include 3/4/5 digits and at least 3 letters
+#  the sum of the digits is in the range 2..15 to set the toggles target
 #  the first three letters should be distinct and in the range 0..4 such that A=0, B=1, etc, to match the jumper wires
 #  the last letter should be outside of the range
 def genSerial():
-    # set the digits (used in the toggle switches phase)
     serial_digits = []
-    toggle_value = randint(1, 15)
-    # the sum of the digits is the toggle value
-    while (len(serial_digits) < 3 or toggle_value - sum(serial_digits) > 0):
-        d = randint(0, min(9, toggle_value - sum(serial_digits)))
-        serial_digits.append(d)
 
-    # set the letters (used in the jumper wires phase)
-    jumper_indexes = [ 0 ] * 5
-    while (sum(jumper_indexes) < 3):
+    # Generate a random toggle value between 2..15 to ensure 'usage' of at least 2 toggles
+    toggle_value = randint(2, 15)
+    print("toggle_value", toggle_value)
+
+    # Randomly choose number of digits (3/4/5)
+    num_digits = randint(3, 5)
+    print("num_digits", num_digits)
+    
+    remaining = toggle_value
+
+    for i in range(num_digits):
+        digits_left = num_digits - i
+
+        # Minimum sum needed for remaining digits (each digit at least 0)
+        min_sum = 0
+        # Maximum value for this digit
+        max_digit = min(9, remaining - min_sum)
+
+        # If it's the last digit, take exactly the remaining
+        if digits_left == 1:
+            d = remaining
+        else:
+            d = randint(0, max_digit)
+        
+        serial_digits.append(d)
+        remaining -= d
+
+    # set the letters (for jumper wires phase)
+    jumper_indexes = [0] * 5
+    while sum(jumper_indexes) < 3:
         jumper_indexes[randint(0, len(jumper_indexes) - 1)] = 1
-    jumper_value = int("".join([ str(n) for n in jumper_indexes ]), 2)
-    # the letters indicate which jumper wires must be "cut"
-    jumper_letters = [ chr(i + 65) for i, n in enumerate(jumper_indexes) if n == 1 ]
+    jumper_value = int("".join([str(n) for n in jumper_indexes]), 2)
+    jumper_letters = [chr(i + 65) for i, n in enumerate(jumper_indexes) if n == 1]
 
     # form the serial number
-    serial = [ str(d) for d in serial_digits ] + jumper_letters
-    # and shuffle it
+    serial = [str(d) for d in serial_digits] + jumper_letters
     shuffle(serial)
-    # finally, add a final letter (F..Z)
-    serial += [ choice([ chr(n) for n in range(70, 91) ]) ]
-    # and make the serial number a string
+    serial += [choice([chr(n) for n in range(70, 91)])]  # F-Z
     serial = "".join(serial)
 
     return serial, toggle_value, jumper_value
